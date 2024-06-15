e22d0b2512870591e8c66dd075d869bdd9938ea5
iggster <ignatius.murillo@gmail.com>
2024-06-14 23:26:01 -0400

    Add mapping.

--- contracts/Todo_List.sol
+++ contracts/Todo_List.sol
@@ -6,4 +6,14 @@ contract TodoList {
   //When public is declared, Solidity automatically creates a 
   //function for access the variable, in this case taskCount.  
   uint public taskCount = 0;
+
+  //Create your data type with struct
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+//This is a mapping in Solidity which stores a hash/key+value. 
+  mapping(unit => Task);
 }
